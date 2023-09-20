8e1fa21869987997f7d2d5b4dcb326c26c9193dd
Valerie Tan <valerietanhx@gmail.com>
2023-09-19 20:40:58 -0400

    feat: add clear button and intro text

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import ClearIcon from "@mui/icons-material/Clear";
 
 function App() {
   const [inputText, setInputText] = useState("");
   const [processedText, setProcessedText] = useState("");
+
+  const clearText = () => {
+    setInputText("");
+  };
 
   const processText = () => {
     const processed = inputText.replace(/\s+/g, " ").trim();
@@ -25,30 +29,50 @@ function App() {
 
   return (
     <Container className="App" maxWidth="md">
-      <h1>Copydf</h1>
+      <h1>COPYDF</h1>
+      <p>
+        You know when you're copy-pasting text from PDFs and the newlines and
+        spaces go all wonky? Yeah. This fixes that.
+      </p>
+      <p>
+        This tool removes all newlines and unnecessary spaces between words.
+      </p>
       <Stack>
         <TextField
+          sx={{ mt: 2 }}
           label="Paste your text here..."
           value={inputText}
           onChange={(e) => setInputText(e.target.value)}
           multiline
           rows={4}
         />
-        <Button
-          sx={{ m: 2 }}
-          className="Button"
-          onClick={processText}
-          variant="contained"
-          endIcon={<SendIcon/>}
-        >
-          <Typography variant="button">
-            Process text
-          </Typography>
-        </Button>
+        <Stack direction="row" className="button-stack">
+          {/*TODO: abstract out Button into a separate component*/}
+          <Button
+            sx={{ m: 2 }}
+            className="Button"
+            onClick={clearText}
+            variant="contained"
+            endIcon={<ClearIcon />}
+          >
+            <Typography variant="button">Clear text</Typography>
+          </Button>
+          <Button
+            sx={{ m: 2 }}
+            className="Button"
+            onClick={processText}
+            variant="contained"
+            endIcon={<SendIcon />}
+          >
+            <Typography variant="button">Process text</Typography>
+          </Button>
+        </Stack>
       </Stack>
       <div>
         <h2>Your processed text will appear here!</h2>
-        <Box sx={{ p: 2, borderRadius: 1, border: "1px solid grey" }}>
+        <Box
+          sx={{ px: 2.5, py: 1, borderRadius: 1, border: "1px solid darkgrey" }}
+        >
           <p className="processed-text">{processedText}</p>
         </Box>
         <Button
@@ -56,11 +80,9 @@ function App() {
           className="Button"
           onClick={copyText}
           variant="contained"
-          endIcon={<ContentCopyIcon/>}
+          endIcon={<ContentCopyIcon />}
         >
-          <Typography variant="button">
-            Copy processed text
-          </Typography>
+          <Typography variant="button">Copy to clipboard</Typography>
         </Button>
       </div>
     </Container>
