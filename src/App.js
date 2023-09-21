88d6d4b7201f308e16b3e5ead625a891bd49f5d9
Valerie Tan <valerietanhx@gmail.com>
2023-09-21 13:54:55 -0400

    feat: add logo and todos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Logo from "./logo.png";
 import React, { useState } from "react";
 import SendIcon from "@mui/icons-material/Send";
 import Button from "@mui/material/Button";
@@ -9,6 +10,7 @@ import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import ClearIcon from "@mui/icons-material/Clear";
+import Link from "@mui/material/Link";
 
 function App() {
   const [inputText, setInputText] = useState("");
@@ -29,6 +31,8 @@ function App() {
 
   return (
     <Container className="App" maxWidth="md">
+      {/*TODO: add theme*/}
+      <img src={Logo} alt="Logo" className="logo" />
       <h1>COPYDF</h1>
       <p>
         You know when you're copy-pasting text from PDFs and the newlines and
@@ -37,6 +41,10 @@ function App() {
       <p>
         This tool removes all newlines and unnecessary spaces between words.
       </p>
+      {/*TODO: add option to correct hyphen spacing - turn on by default*/}
+      {/*TODO: add ML to detect and handle e.g. "whenitcopieslikethis", "o r t h i s"*/}
+      {/*TODO: make text boxes side by side*/}
+      {/*TODO: use Grid instead*/}
       <Stack>
         <TextField
           sx={{ mt: 2 }}
@@ -85,6 +93,27 @@ function App() {
           <Typography variant="button">Copy to clipboard</Typography>
         </Button>
       </div>
+      <footer className="footer">
+        {/*TODO: add github repo link*/}
+        <p>
+          © 2023 by{" "}
+          <Link href="https://github.com/valerietanhx" target="_blank">
+            valerietanhx
+          </Link>
+          .
+        </p>
+
+        <p>
+          Favicon by&nbsp;
+          <Link
+            href="https://www.flaticon.com/free-animated-icon/pdf-file_10826752"
+            target="_blank"
+          >
+            Freepik - Flaticon
+          </Link>
+          .
+        </p>
+      </footer>
     </Container>
   );
 }
