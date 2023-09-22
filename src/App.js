cef47f5dba96047c2f965120d98c351076b93125
Valerie Tan <valerietanhx@gmail.com>
2023-09-22 15:19:30 -0400

    feat: redesign entire website
    - textboxes side by side
    - responsiveness
    - processed text box changed to textfield
    - background colours changed

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import React, { useState } from "react";
 import SendIcon from "@mui/icons-material/Send";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import ClearIcon from "@mui/icons-material/Clear";
@@ -30,91 +27,94 @@ function App() {
   };
 
   return (
-    <Container className="App" maxWidth="md">
+    <div className="App">
       {/*TODO: add theme*/}
-      <img src={Logo} alt="Logo" className="logo" />
-      <h1>COPYDF</h1>
-      <p>
-        You know when you're copy-pasting text from PDFs and the newlines and
-        spaces go all wonky? Yeah. This fixes that.
-      </p>
-      <p>
-        This tool removes all newlines and unnecessary spaces between words.
-      </p>
-      {/*TODO: add option to correct hyphen spacing - turn on by default*/}
-      {/*TODO: add ML to detect and handle e.g. "whenitcopieslikethis", "o r t h i s"*/}
-      {/*TODO: make text boxes side by side*/}
-      {/*TODO: use Grid instead*/}
-      <Stack>
-        <TextField
-          sx={{ mt: 2 }}
-          label="Paste your text here..."
-          value={inputText}
-          onChange={(e) => setInputText(e.target.value)}
-          multiline
-          rows={4}
-        />
-        <Stack direction="row" className="button-stack">
-          {/*TODO: abstract out Button into a separate component*/}
-          <Button
-            sx={{ m: 2 }}
-            className="Button"
-            onClick={clearText}
-            variant="contained"
-            endIcon={<ClearIcon />}
-          >
-            <Typography variant="button">Clear text</Typography>
-          </Button>
-          <Button
-            sx={{ m: 2 }}
-            className="Button"
-            onClick={processText}
-            variant="contained"
-            endIcon={<SendIcon />}
-          >
-            <Typography variant="button">Process text</Typography>
-          </Button>
-        </Stack>
-      </Stack>
-      <div>
-        <h2>Your processed text will appear here!</h2>
-        <Box
-          sx={{ px: 2.5, py: 1, borderRadius: 1, border: "1px solid darkgrey" }}
-        >
-          <p className="processed-text">{processedText}</p>
-        </Box>
-        <Button
-          sx={{ m: 2 }}
-          className="Button"
-          onClick={copyText}
-          variant="contained"
-          endIcon={<ContentCopyIcon />}
-        >
-          <Typography variant="button">Copy to clipboard</Typography>
-        </Button>
+      <div className="left">
+        <img src={Logo} alt="Logo" className="logo" />
+        <h1>COPYDF</h1>
+        <p>
+          You know when you're copy-pasting text from PDFs and the newlines and
+          spaces go all wonky? Yeah. This fixes that.
+        </p>
+        <p>
+          This tool removes all newlines and unnecessary spaces between words.
+        </p>
+        <div className="text-buttons-stack">
+          {/*TODO: add option to correct hyphen spacing - turn on by default*/}
+          {/*TODO: add ML to detect and handle e.g. "whenitcopieslikethis", "o r t h i s"*/}
+          <TextField
+            sx={{ mt: 2 }}
+            label="Paste your text here..."
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            multiline
+            rows={6}
+          />
+          <div class="process-text-buttons">
+            {/*TODO: abstract out Button into a separate component*/}
+            <Button
+              sx={{ mx: 0, my: 2 }}
+              onClick={clearText}
+              variant="outlined"
+              endIcon={<ClearIcon />}
+            >
+              <Typography variant="button">Clear text</Typography>
+            </Button>
+            <Button
+              sx={{ mx: 0, my: 2 }}
+              onClick={processText}
+              variant="contained"
+              endIcon={<SendIcon />}
+            >
+              <Typography variant="button">Process text</Typography>
+            </Button>
+          </div>
+        </div>
       </div>
-      <footer className="footer">
-        {/*TODO: add github repo link*/}
-        <p>
-          © 2023 by{" "}
-          <Link href="https://github.com/valerietanhx" target="_blank">
-            valerietanhx
-          </Link>
-          .
-        </p>
-
-        <p>
-          Favicon by&nbsp;
-          <Link
-            href="https://www.flaticon.com/free-animated-icon/pdf-file_10826752"
-            target="_blank"
+      <div className="right">
+        <h2>Your processed text will appear here!</h2>
+        <div className="text-buttons-stack">
+          <TextField
+            sx={{ mt: 2 }}
+            label="Processed text..."
+            value={processedText}
+            onChange={(e) => setProcessedText(e.target.value)}
+            InputProps={{
+              readOnly: true,
+            }}
+            multiline
+            rows={6}
+          />
+          <Button
+            sx={{ mx: 0, my: 2 }}
+            onClick={copyText}
+            variant="contained"
+            endIcon={<ContentCopyIcon />}
           >
-            Freepik - Flaticon
-          </Link>
-          .
-        </p>
-      </footer>
-    </Container>
+            <Typography variant="button">Copy to clipboard</Typography>
+          </Button>
+          <footer>
+            <p>
+              © 2023 by{" "}
+              <Link
+                href="https://github.com/valerietanhx/copydf"
+                target="_blank"
+              >
+                valerietanhx
+              </Link>
+              . Favicon and logo by&nbsp;
+              <Link
+                href="https://www.flaticon.com/free-animated-icon/pdf-file_10826752"
+                target="_blank"
+              >
+                Freepik - Flaticon
+              </Link>
+              .
+            </p>
+          </footer>
+        </div>
+      </div>
+    </div>
   );
 }
 
